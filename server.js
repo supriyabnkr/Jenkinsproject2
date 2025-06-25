a13732501af833a159382f62b47ed2ee3e521212
supriyabnkr <supriyabnkr@gmail.com>
2025-06-25 16:42:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello This is devops supriya please follow mrrrrrrrrrrrrrr===================== .........');
+  res.send('Hello This is devops supriya .........');
 });
 
 app.listen(PORT, HOST);
